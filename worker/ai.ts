169b250982b1a0c631b20961b49e9097a5d74ff5
Benjamín Vicente <benjavicente@uc.cl>
2025-04-25 19:44:18 -0400

    ai json

---
+++ worker/ai.ts
@@ -0,0 +1,75 @@
+import { Schema, Type } from "@google/genai";
+
+export const systemInstruction = () => `
+Debes tomar los archivos adjuntos y el contenido del usuario y resumir los movimientos bancarios.
+Para esto, es necesario que que extraigas información de las transacciones bancarias, pagos y compeas.
+
+Debes entregar los siguientes datos:
+- Día
+- Mes
+- Año
+- Monto, en formato float
+- Nombre del movimiento raw: el nombre original del movimiento
+- Nombre del movimiento: el nombre limpiado, eliminando información necesaria.
+- Categoría: en que clasifica el movimiento, dependiendo de las categorías.
+- Tipo: Si es un gasto o ingreso.
+- Identificador de la tarjeta o cuenta: usualmente es los últimos 4 dígitos de la tarjeta o cuenta.
+- Moneda: la moneda en la que se realizó el movimiento. Puede ser CLP, USD, EUR, etc.
+
+Las categorías son las siguientes:
+- Comida
+- Entretenimiento
+- Salud
+- Transporte
+- Hogar
+- Inversión
+- Sueldo
+- Transferencia
+- Otros
+
+Si tienes la herramienta, puedes buscar en internet para identificar la categoría.
+
+Para limpiar el nombre, debes eliminar información necesaria y dejar solo lo relevante.
+Por ejemplo:
+
+1. PAGO:MERPAGO*LAGUNA PI -> Laguna Pi.
+No es relevanto dejar que es un pago, ni mercado pago, que es el medio de pago.
+
+2. AGO:SUC VITACURA 5321 -> Sucursal Vitacura 5321
+Expandir abreviaciones.
+
+3. MIT Burger -> MIT Burger
+No dejar en minúsculas abreviaciones desconocidas.
+
+El esquema de cada transferencia es el siguiente:
+
+${JSON.stringify(schema)}
+`;
+
+export const schema: Schema = {
+  type: Type.OBJECT,
+  properties: {
+    day: { type: Type.INTEGER },
+    month: { type: Type.INTEGER },
+    year: { type: Type.INTEGER },
+    amount: { type: Type.NUMBER },
+    raw_transaction_name: { type: Type.STRING },
+    clean_transaction_name: { type: Type.STRING },
+    category: { type: Type.STRING },
+    type: { type: Type.STRING },
+    account_identifier: { type: Type.STRING },
+    currency: { type: Type.STRING },
+  },
+  required: [
+    "day",
+    "month",
+    "year",
+    "amount",
+    "raw_transaction_name",
+    "clean_transaction_name",
+    "category",
+    "type",
+    "account_identifier",
+    "currency",
+  ],
+} as const;
--- worker/index.ts
+++ worker/index.ts
@@ -7,6 +7,7 @@ import { EmailMessage } from "cloudflare:email";
 import initWASM, { remove_pdf_password } from "../pdf_password_remover/pkg/pdf_password_remover";
 import wasm from "../pdf_password_remover/pkg/pdf_password_remover_bg.wasm";
 import { Buffer } from "node:buffer";
+import { systemInstruction } from "./ai";
 
 const emailRoute = "robots@tiuke.money";
 let wasmReady = false;
@@ -25,82 +26,87 @@ export default {
   // ignore unused ctx
 
   async email(message, env, _ctx) {
-    if (!wasmReady) {
-      await initWASM(wasm);
-      wasmReady = true;
-    }
-
-    const ai = new GoogleGenAI({ apiKey: env.GOOGLE_AI_API_KEY });
-
-    const rawEmail = new Response(message.raw);
-    const email = await PostalMine.parse(await rawEmail.arrayBuffer());
-
-    console.info("atachments", JSON.stringify(email.attachments));
-    const base64Files: { data: string; mimeType: string }[] = [];
-    for (const attachment of email.attachments) {
-      console.info("attachment", attachment.filename);
-      if (attachment.mimeType === "application/pdf") {
-        const pdf = attachment;
-        let pdfBuffer = pdf.content;
-        if (typeof pdfBuffer === "string") {
-          console.info("pdf buffer is string");
-          pdfBuffer = Buffer.from(pdfBuffer, "base64");
-        } else {
-          console.info("pdf buffer is not string");
-          pdfBuffer = Buffer.from(pdfBuffer);
-        }
-
-        console.info("extracting pdf buffer");
-        const pdfWithoutPassword = await remove_pdf_password(new Uint8Array(pdfBuffer), ["9910"]);
-        const file = Buffer.from(pdfWithoutPassword).toString("base64");
-        console.info("pdf buffer extracted");
-        base64Files.push({ data: file, mimeType: pdf.mimeType });
+    try {
+      if (!wasmReady) {
+        await initWASM(wasm);
+        wasmReady = true;
       }
-    }
 
-    const content = email.html ?? email.text;
+      const ai = new GoogleGenAI({ apiKey: env.GOOGLE_AI_API_KEY });
 
-    if (!content) throw new Error("No content found");
+      const rawEmail = new Response(message.raw);
+      const email = await PostalMine.parse(await rawEmail.arrayBuffer());
 
-    console.log("email from", message.from);
-    const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
-      contents: [
-        {
-          parts: [
-            ...base64Files.map((file) => ({
-              inlineData: {
-                data: file.data,
-                mimeType: file.mimeType,
+      console.info("atachments", JSON.stringify(email.attachments));
+      const base64Files: { data: string; mimeType: string }[] = [];
+      for (const attachment of email.attachments) {
+        console.info("attachment", attachment.filename);
+        if (attachment.mimeType === "application/pdf") {
+          const pdf = attachment;
+          let pdfBuffer = pdf.content;
+          if (typeof pdfBuffer === "string") {
+            console.info("pdf buffer is string");
+            pdfBuffer = Buffer.from(pdfBuffer, "base64");
+          } else {
+            console.info("pdf buffer is not string");
+            pdfBuffer = Buffer.from(pdfBuffer);
+          }
+
+          console.info("extracting pdf buffer");
+          const pdfWithoutPassword = await remove_pdf_password(new Uint8Array(pdfBuffer), ["9910"]);
+          const file = Buffer.from(pdfWithoutPassword).toString("base64");
+          console.info("pdf buffer extracted");
+          base64Files.push({ data: file, mimeType: pdf.mimeType });
+        }
+      }
+
+      const content = email.html ?? email.text;
+
+      if (!content) throw new Error("No content found");
+
+      console.log("email from", message.from);
+      const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: [
+          {
+            parts: [
+              ...base64Files.map((file) => ({
+                inlineData: {
+                  data: file.data,
+                  mimeType: file.mimeType,
+                },
+              })),
+              {
+                text: content,
               },
-            })),
-            {
-              text: content,
-            },
-          ],
+            ],
+          },
+        ],
+        config: {
+          systemInstruction: systemInstruction(),
         },
-      ],
-      config: {
-        systemInstruction: "Entrega los detalles del mobimiento del banco, incluyendo fecha, monto, nomnre y categoría",
-      },
-    });
+      });
 
-    console.info(JSON.stringify(response));
+      console.info(JSON.stringify(response));
 
-    const msg = createMimeMessage();
-    msg.setSender({ name: "Tiuke Robots", addr: emailRoute });
-    msg.setHeader("In-Reply-To", message.headers.get("Message-ID") ?? "");
-    msg.setRecipient(message.from);
-    msg.setSubject("Re: " + email.subject);
-    msg.addMessage({
-      contentType: "text/plain",
-      data: response.text!,
-    });
+      const msg = createMimeMessage();
+      msg.setSender({ name: "Tiuke Robots", addr: emailRoute });
+      msg.setHeader("In-Reply-To", message.headers.get("Message-ID") ?? "");
+      msg.setRecipient(message.from);
+      msg.setSubject("Re: " + email.subject);
+      msg.addMessage({
+        contentType: "text/plain",
+        data: response.text!,
+      });
 
-    console.info("msg", JSON.stringify(msg.headers));
+      console.info("msg", JSON.stringify(msg.headers));
 
-    const replyMessage = new EmailMessage(emailRoute, message.from, msg.asRaw());
+      const replyMessage = new EmailMessage(emailRoute, message.from, msg.asRaw());
 
-    await message.reply(replyMessage);
+      await message.reply(replyMessage);
+    } catch (error) {
+      console.error("Error processing email", error);
+      // TODO: handle error
+    }
   },
 } satisfies ExportedHandler<Env>;
